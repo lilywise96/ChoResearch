918ca3092c0747c1450ee3e3d23c819fdbcdc84f
LilyW <Lily_Wise@baylor.edu>
2019-10-09 18:03:01 -0500

    Add comments for association creation.

--- association_creation.py
+++ association_creation.py
@@ -15,6 +15,7 @@ from math import ceil
 def coverage(left_val, all_itemsets):
     count = 0
 
+    # Loop through all the itemsets and check if the left_val is in the itemset
     for itemset in all_itemsets:
         if left_val in itemset:
             count += 1
@@ -22,20 +23,12 @@ def coverage(left_val, all_itemsets):
     return float(count/len(all_itemsets))
 
 
-def create_associations(all_gt, freq_itemsets, min_confidence, min_coverage):
-    final_associations = []
-    associations = all_associations(freq_itemsets)
-    min_confidence = ceil(min_confidence * len(freq_itemsets))
-
-    for associate in associations:
-        cur_confidence = confidence(all_gt, associate)
-        cur_coverage = coverage(associate[0], all_gt)
-        if cur_confidence > min_confidence and cur_coverage > min_coverage:
-            final_associations.append(associate)
-
-    return final_associations
-
-
+# This functions calculates the confidence of a given association.
+#
+# param: all_gt - all of the itemsets
+# param: association - the current association to calculate confidence for
+#
+# returns: the confidence count as a decimal
 def confidence(all_gt, association):
     confidence_count = 0
 
@@ -56,9 +49,38 @@ def confidence(all_gt, association):
         if found_big:
             confidence_count += 1
 
-    return confidence_count
+    return float(confidence_count / len(all_gt))
 
 
+# This function takes the frequent itemsets that were created by the apriori algorithm and creates associations. An
+# association is kept if it meets the minimum confidence requirements and the left side of the association meets the
+# minimum coverage requirements.
+#
+# param: all_gt - all the itemsets originally read in
+# param: freq_itemsets - the frequent itemsets created by the apriori algorithm
+# param: min_confidence - the minimum confidence, as a decimal
+# param: min_coverage - the minimum coverage, as a decimal
+#
+# returns: the list of final associations that meets the requirements
+def create_associations(all_gt, freq_itemsets, min_confidence, min_coverage):
+    final_associations = []
+    associations = all_associations(freq_itemsets)
+    min_confidence = ceil(min_confidence * len(freq_itemsets))
+
+    for associate in associations:
+        cur_confidence = confidence(all_gt, associate)
+        cur_coverage = coverage(associate[0], all_gt)
+        if cur_confidence > min_confidence and cur_coverage > min_coverage:
+            final_associations.append(associate)
+
+    return final_associations
+
+
+# Creates all possible associations with the frequent itemsets.
+#
+# param: freq_itemsets - the list of frequent itemsets created by the apriori algorithm
+#
+# returns: the associations created.
 def all_associations(freq_itemsets):
     associations = []
     for itemset in freq_itemsets:
